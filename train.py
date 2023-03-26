5d7fb7d150cb55e6d6781d402d02c719c7fb69e0
pawelczapla <pawelczapla2099@gmail.com>
2023-03-26 22:45:05 +0200

    Name changes, clean-up

--- train.py
+++ train.py
@@ -24,22 +24,22 @@ def train(args, model, device, train_loader, optimizer, criterion, epoch):
                 break
 
 
-def test(model, device, test_loader):
+def evaluate(model, device, criterion, test_loader):
     model.eval()
-    test_loss = 0
+    val_loss = 0
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += nn.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            val_loss += criterion(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
 
-    test_loss /= len(test_loader.dataset)
+    val_loss /= len(test_loader.dataset)
 
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
+        val_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
 
@@ -85,7 +85,7 @@ def main():
 
     for epoch in range(1, epochs + 1):
         train(args, model, device, train_loader, optimizer, criterion, epoch)
-        test(model, device, test_loader)
+        evaluate(model, device, criterion, test_loader)
         scheduler.step()
 
     if args.save_model:
